ab0d7c1ccc7733a573a33d94f666b42080c164f6
manases24 <hugo.orlando2050@gmail.com>
2024-12-04 20:58:51 -0300

    MyCounterScript - composables

--- 03-indecision-app/src/composables/useCounter.ts
+++ 03-indecision-app/src/composables/useCounter.ts
@@ -1,7 +1,5 @@
 import { ref, computed } from 'vue'
 
-// const counter = ref(10);
-
 export const useCounter = (initialValue: number = 5) => {
   const counter = ref(initialValue)
   const squareCounter = computed(() => counter.value * counter.value)
