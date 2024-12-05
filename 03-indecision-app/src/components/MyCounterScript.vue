9c3d358d7f46f9393a5a452d4f1b2ea110c7598c
manases24 <hugo.orlando2050@gmail.com>
2024-12-04 21:01:07 -0300

    MyCounterScript - composables

--- 03-indecision-app/src/components/MyCounterScript.vue
+++ 03-indecision-app/src/components/MyCounterScript.vue
@@ -5,13 +5,13 @@
     <h3>Square:{{ squareCounter }}</h3>
 
     <div>
-      <button @click="counter++">+1</button>
-      <button @click="counter--">-1</button>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { useCounter } from '../composables/useCounter'
-const { counter, squareCounter } = useCounter()
+const { counter, squareCounter, increment, decrement } = useCounter()
 </script>
--- 03-indecision-app/src/composables/useCounter.ts
+++ 03-indecision-app/src/composables/useCounter.ts
@@ -3,9 +3,13 @@ import { ref, computed } from 'vue'
 export const useCounter = (initialValue: number = 5) => {
   const counter = ref(initialValue)
   const squareCounter = computed(() => counter.value * counter.value)
+  const increment = () => counter.value++
+  const decrement = () => counter.value--
 
   return {
     counter,
     squareCounter,
+    increment,
+    decrement,
   }
 }
