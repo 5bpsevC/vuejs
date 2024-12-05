1a40a07ccd92b3451c35eecbe598012c7a9e3d13
manases24 <hugo.orlando2050@gmail.com>
2024-12-05 19:16:52 -0300

    pokemon game

---
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,16 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-pulse">Cargando Pokemon</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Quien es este Pokemon?</h1>
+
+    <!-- Pokemon Picture -->
+
+    <!-- Pokemon Options -->
+  </section>
+</template>
+
+<script setup lang="ts"></script>
