d196eae894f89ec32859da2224e78a5362261f5c
mana <manashe.tsx@gmail.com>
2025-03-25 14:31:58 -0300

    counter component

---
+++ 03-indecision-app/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+// const props = defineProps({
+//   value: { type: Number, required: true },
+// });
+
+// const props = defineProps<{ value: number }>();
+
+interface Props {
+  value: number;
+}
+
+const props = defineProps<Props>();
+
+const counter = ref(props.value);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
