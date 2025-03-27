2005c4679287d7ac5a1b65d79e7b2cfbee1ee85a
mana <manashe.tsx@gmail.com>
2025-03-27 13:10:33 -0300

    counter component

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Counter from './components/Counter.vue';
 import CounterScript from './components/CounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/CounterScript2.vue'; 
 </script>
 
 <template>
   <h1>Mi primera app</h1>
-  <Counter :value="5" />
-  <CounterScript :value="5"/>
+  <Counter :value="1" />
+  <CounterScript :value="2"/>
+  <MyCounterScript2 :value="3" />
 </template>
---
+++ 03-indecision-app/src/components/my-counter-script/CounterScript2.vue
@@ -0,0 +1,13 @@
+<script lang="ts" src="./MyCounterScript2.ts"></script>
+
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
