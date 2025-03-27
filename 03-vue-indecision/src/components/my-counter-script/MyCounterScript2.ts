a0f7b60c46affa566ce7ffabd726b6d1eafbcf79
mana <manashe.tsx@gmail.com>
2025-03-27 15:29:14 -0300

    vue indecision

--- 03-vue-indecision/src/components/my-counter-script/MyCounterScript2.ts
+++ 03-vue-indecision/src/components/my-counter-script/MyCounterScript2.ts
@@ -1,5 +1,5 @@
-import { useCounter } from '@/composables/useCounter';
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { useCounter } from "@/composables/useCounter";
 
 export default defineComponent({
   props: {
