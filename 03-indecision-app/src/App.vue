9bcae3c05370db0d1a10c0d50e181e89a7ffe5ef
mana <manashe.tsx@gmail.com>
2025-03-27 12:50:49 -0300

    counter component

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Counter from './components/Counter.vue';
+import CounterScript from './components/CounterScript.vue';
 </script>
 
 <template>
   <h1>Mi primera app</h1>
   <Counter :value="5" />
+  <CounterScript :value="5"/>
 </template>
---
+++ 03-indecision-app/src/components/CounterScript.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const counter = ref(props.value);
+    const squareCounter = computed(() => counter.value * counter.value);
+
+    return {
+      counter,
+      squareCounter,
+    };
+  },
+});
+</script>
+
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
