125dbf04c1415d12f021123e094e7d84e2ac2459
manatsx <manashe.tsx@gmail.com>
2025-05-24 16:35:12 +0000

    indecision app

---
+++ 04-vue-indecision/src/components/chat/ChatBubble.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  message: string;
+  itsMine: boolean;
+  image?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex justify-end" v-if="itsMine">
+    <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
+      {{ message }}
+    </div>
+  </div>
+
+  <div class="flex" v-else>
+    <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+      <span class="capitalize">{{ message }}</span>
+      <img
+        class="w-52 h-52 object-cover rounded-md"
+        v-if="image"
+        :src="image"
+        alt="YesNoImage"
+      />
+    </div>
+  </div>
+</template>
---
+++ 04-vue-indecision/src/components/chat/ChatMessages.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ChatBubble from "./ChatBubble.vue";
+</script>
+
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <!-- Messages go here -->
+      <ChatBubble :its-mine="true" message="Hello, World!" />
+
+      <ChatBubble :its-mine="false" message="'no'" image="https://yesno.wtf/assets/no/24-159febcfd655625c38c147b65e5be565.gif"/>
+    </div>
+  </div>
+</template>
