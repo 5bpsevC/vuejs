11103447eab31f78c1294f65909aa1dca0956d64
manatsx <manashe.tsx@gmail.com>
2025-05-24 21:18:41 +0000

    pokemon game

--- 05-pokemon-game/src/modules/pokemon/components/PokemonOptions.vue
+++ 05-pokemon-game/src/modules/pokemon/components/PokemonOptions.vue
@@ -1,12 +1,38 @@
-<script setup lang="ts"></script>
-
 <template>
   <section class="mt-5 flex flex-col">
     <button
-      class="capitalize disabled:shadow-none disabled:bg-gray-100"
-    ></button>
+      v-for="{ name, id } in options"
+      :key="id"
+      @click="$emit('selectedOption', id)"
+      :class="[
+        'capitalize disabled:shadow-none disabled:bg-gray-100',
+        {
+          correct: id === correctAnswer && blockSelection,
+          incorrect: id !== correctAnswer && blockSelection,
+        },
+      ]"
+      :disabled="blockSelection"
+    >
+      {{ name }}
+    </button>
   </section>
 </template>
+
+<script setup lang="ts">
+import type { Pokemon } from '../interfaces';
+
+interface Props {
+  options: Pokemon[];
+  blockSelection: boolean;
+  correctAnswer: number;
+}
+
+defineProps<Props>();
+
+defineEmits<{
+  selectedOption: [id: number];
+}>();
+</script>
 
 <!-- <style scoped>
 button {
