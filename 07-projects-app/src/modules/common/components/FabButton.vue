44aae107e0325d5a8d9bcec3289da266ed5685eb
5bpsevC <brite6001@gmail.com>
2025-10-13 16:03:19 +0000

    pinia

---
+++ 07-projects-app/src/modules/common/components/FabButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+}
+
+withDefaults(defineProps<Props>(), {
+  position: 'bottom-right',
+});
+</script>
+
+<template>
+  <button class="btn btn-info">Info</button>
+</template>
